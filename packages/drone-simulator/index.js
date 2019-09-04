0631109cc732f9ba0470545b99b749c3c793ffb8
faroutchris <georgievchristo@gmail.com>
2019-09-04 13:03:36 +0200

    feat: Calculate distance between coordinates

--- packages/drone-simulator/index.js
+++ packages/drone-simulator/index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
+
 app.post('/init', drone.init)
 
 app.listen(port, () => console.log(`Service running on ${port}!🚀`))
--- packages/drone-simulator/services/drone.js
+++ packages/drone-simulator/services/drone.js
@@ -1,10 +1,31 @@
 const got = require('got')
+const directions = require('./../directions');
 
-async function init({ body: { start, stop, webhookUrl } }) {
-  console.log(start, stop, webhookUrl)
+async function init({ body: { start, stop, webhookUrl } }, res) {
+  try {
+    console.log(start, stop, webhookUrl);
+
+    const webhookRes = await sendDroneStatus(webhookUrl, {
+      start,
+      stop,
+      currentPos: start,
+      distance: directions.getDistance(start, stop),
+      bearing: 0,
+      status: "initiating",
+      vehicle: "Drone",
+      batteryStatus: 1000
+    });
+
+    res.json({ status: 'OK' });
+  } catch (error) {
+    res.json({ status: "ERROR", error });
+  }
 }
 
-async function sendStatus(webhookUrl) {}
+async function sendDroneStatus(webhookUrl, postBody) {
+  return got(webhookUrl, { body: postBody, json: true });
+  //return "OK"
+}
 
 module.exports = {
   init
