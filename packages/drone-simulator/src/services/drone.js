c8d0c4ace23e5931c5363e235a43ac98bb8ca3e4
Jimmy Jardland <jimmy@jardland.se>
2019-09-04 13:16:53 +0200

    feature: drone status

--- packages/drone-simulator/src/services/drone.js
+++ packages/drone-simulator/src/services/drone.js
@@ -1,17 +1,30 @@
 const got = require('got')
+const directions = require('./../directions')
 
-async function init({ body: { start, stop, webhookUrl } }) {
-  console.log(start, stop, webhookUrl)
+async function init({ body: { start, stop, webhookUrl } }, res) {
+  try {
+    console.log(start, stop, webhookUrl)
+
+    const webhookRes = await sendDroneStatus(webhookUrl, {
+      start,
+      stop,
+      currentPos: start,
+      distance: directions.getDistance(start, stop),
+      bearing: 0,
+      status: 'initiating',
+      vehicle: 'Drone',
+      batteryStatus: 1000,
+    })
+
+    res.json({ status: 'OK' })
+  } catch (error) {
+    res.json({ status: 'ERROR', error })
+  }
 }
 
-async function sendStatus(webhookUrl, body) {
-  return got.post(webhookUrl, body)
-}
-
-async function moveDrone(coords) {
-  //move drone
-
-  sendStatus()
+async function sendDroneStatus(webhookUrl, postBody) {
+  return got(webhookUrl, { body: postBody, json: true })
+  //return "OK"
 }
 
 module.exports = {
