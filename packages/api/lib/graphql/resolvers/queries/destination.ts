90e423e59159f7f6b65b8750ea08ef7c8ee248bb
Jimmy Jardland <jimmy@jardland.se>
2019-09-18 16:04:41 +0200

    chore(api): no unused vars or params

--- packages/api/lib/graphql/resolvers/queries/destination.ts
+++ packages/api/lib/graphql/resolvers/queries/destination.ts
@@ -1,6 +1,6 @@
 import { QueryResolvers } from '../../../__generated__/brevduvor'
 
-export const allDestinations: QueryResolvers['allDestinations'] = (_, args) => {
+export const allDestinations: QueryResolvers['allDestinations'] = _ => {
   return [
     { alias: 'Storuman', lat: 65.090833, lon: 17.1075 },
     { alias: 'Kvikkjokk', lat: 66.9501067, lon: 17.70861 },
