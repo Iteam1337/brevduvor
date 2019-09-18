2567d89fc57667bad6d9a2e68d8624ce0429ef95
Jimmy Jardland <jimmy@jardland.se>
2019-09-18 14:19:41 +0200

    chore(api): remove cl

--- packages/api/lib/services/droneStatus.ts
+++ packages/api/lib/services/droneStatus.ts
@@ -2,7 +2,6 @@ import pubsub from '../adapters/pubsub'
 import { saveDroneStatus } from './elastic'
 
 export const droneStatus = ({ body: dronePosition }, res, next) => {
-  console.log(dronePosition)
   saveDroneStatus({
     position: dronePosition,
   })
