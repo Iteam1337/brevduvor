2213e4988ee344339f466ed621c76eb1e29d8351
momentiris <momentiris@gmail.com>
2019-10-07 16:29:21 +0200

    fix: return drone_id, not id

--- packages/api/lib/graphql/resolvers/queries/drones.ts
+++ packages/api/lib/graphql/resolvers/queries/drones.ts
@@ -4,5 +4,5 @@ import { getAllDrones } from '../../../services/drones'
 export const drones: QueryResolvers['drones'] = async () => {
   const drones = await getAllDrones()
 
-  return drones.map(({ id }) => id)
+  return drones.map(({ drone_id }) => drone_id)
 }
