4d25abb6d58159e40494ac746593df3c4c19914e
Jimmy Jardland <jimmy@jardland.se>
2019-10-10 11:11:54 +0200

    test: tests for mutations

--- packages/api/lib/graphql/resolvers/mutations/__tests__/initDrone.spec.ts
+++ packages/api/lib/graphql/resolvers/mutations/__tests__/initDrone.spec.ts
@@ -1,21 +1,54 @@
 import { initDrone } from '../initDrone'
+import { dronePost } from '../../../../adapters/drone'
+import { insertDroneTrip } from '../../../../services/drones'
 
-// const args = {
-//   start: {
-//     alias: 'Storuman',
-//     lat: 1.1234,
-//     lon: 1.1234,
-//   },
-//   stop: {
-//     alias: 'Slussfors',
-//     lat: 1.1234,
-//     lon: 1.1234,
-//   },
-// }
+const dronePostResponse = {
+  body: {
+    id: 1337,
+    start: { lat: 1, lon: 2 },
+    stop: { lat: 3, lon: 4 },
+  },
+}
 
-describe('initDrone', () => {
-  it('exists', async () => {
-    //expect(await initDrone({}, args, {}, {} as any)).toHaveProperty('waypoints')
-    expect(initDrone).toBeDefined()
+const input = { start: { lat: 1, lon: 2 }, stop: { lat: 3, lon: 4 } }
+
+jest.mock('../../../../adapters/drone', () => ({
+  dronePost: jest.fn(() => dronePostResponse),
+}))
+
+jest.mock('../../../../services/drones', () => ({
+  insertDroneTrip: jest.fn(),
+}))
+
+describe('#initDrone', () => {
+  it('starts the drone through the api', async () => {
+    await initDrone({} as any, input as any, {} as any, {} as any)
+
+    expect(dronePost).toBeCalledWith('/init', input)
+  })
+
+  it('saves the trip down to the database', async () => {
+    await initDrone({} as any, input as any, {} as any, {} as any)
+
+    expect(insertDroneTrip).toBeCalledWith({
+      drone_id: dronePostResponse.body.id,
+      status: 'initating',
+      start: `(${dronePostResponse.body.start.lat}, ${dronePostResponse.body.start.lon})`,
+      stop: `(${dronePostResponse.body.stop.lat}, ${dronePostResponse.body.stop.lon})`,
+      finished: false,
+      allowed_spectators: [],
+    })
+  })
+
+  it('returns the post body from the drone', async () => {
+    const body = await initDrone({} as any, input as any, {} as any, {} as any)
+    expect(body).toEqual(dronePostResponse.body)
+  })
+
+  it('throws an error if it cant connect to the drone', async () => {
+    ;(dronePost as jest.Mock).mockRejectedValueOnce('err')
+    expect(
+      initDrone({} as any, input as any, {} as any, {} as any)
+    ).rejects.toThrow('Error in initDrone: err')
   })
 })
