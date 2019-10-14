00e8a21311bfd429404de0f8aea6e50a39a01ab0
Jimmy Jardland <jimmy@jardland.se>
2019-10-14 10:33:32 +0200

    chore: ignoreCodes T2722

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts', 'node', 'd.ts'],
   globals: {
     'ts-jest': {
-      diagnostics: false,
+      diagnostics: { ignoreCodes: 'TS2722' },
     },
   },
   watchPlugins: [
