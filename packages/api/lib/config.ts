e51ed4c6474a49c720c163bdf9806c12b9a93d6b
momentiris <momentiris@gmail.com>
2019-11-04 12:47:29 +0100

    chore: use envs in accordance with how @iteam/config works

--- packages/api/lib/config.ts
+++ packages/api/lib/config.ts
@@ -2,21 +2,19 @@ const config = require('@iteam/config')({
   file: `${__dirname}/../config.json`,
   defaults: {
     PORT: 4000,
-    DRONE_URL: process.env.DRONE_URL || 'http://localhost:3030',
-    OSRM_URL: process.env.OSRM_HOST || 'http://localhost:5000',
-    ELASTIC_URL: process.env.ELASTIC_HOST || 'http://localhost:9200',
-    WEBHOOK_URL: process.env.WEBHOOK_URL || 'http://localhost:4000/status',
+    DRONE_URL: 'http://localhost:3030',
+    OSRM_URL: 'http://localhost:5000',
+    ELASTIC_URL: 'http://localhost:9200',
+    WEBHOOK_URL: 'http://localhost:4000/status',
     POSTGRES: {
       USER: 'iteamadmin',
-      PASSWORD: process.env.POSTGRES_PASSWORD || 'adminadmin1337',
+      PASSWORD: 'adminadmin1337',
       DATABASE: 'brevduvor',
       PORT: 5432,
-      HOST: process.env.POSTGRES_HOST || 'localhost',
+      HOST: 'localhost',
       MAX: 20,
       timeout: 30000,
     },
-    JWT_PRIVATE_KEY:
-      'wgQuABVcobsQhcFv1XjflYPRAwZRU-cvzVjjk-dv7rz2yOXIstOxgi30rKLTZzVyfHYbQNw-UqYfI1IZYiZHv1fuN2muTqwUH5LKzfp_DwxuVyGbdZa9lBwmJbDeSwciStsyCylonFYyUCCr4k-Jg2hr68fo3dl-SRYQlDq_gRSY2Irtoyve_XKO_OvSxA3HRztdJJb9AyaTAi9_3O617j4oTcFIMaW80n8QATWCzXlm2QWU4sK5odw-MDK83WJuJ20p6iYz1dHWL2Avw6CB8Y0fq0kEw4wUZJXt1IYi42ZXZDKxl-goHzE8gBhCdeRsnLtVilHGGIZKM3QEkRXgYQ',
   },
 })
 
