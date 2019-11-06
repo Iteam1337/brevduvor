ca3bdf4750089891e8e061bdfbbc2a723b715ac6
faroutchris <georgievchristo@gmail.com>
2019-11-06 10:21:35 +0100

    fix(api): query erroring out

--- packages/api/lib/services/notifications.ts
+++ packages/api/lib/services/notifications.ts
@@ -2,14 +2,13 @@ import { pgp, db } from './../adapters/postgres'
 import { NotificationInput } from '../__generated__/brevduvor'
 
 export async function createNew(notification: NotificationInput) {
-  const query =
-    pgp.helpers.insert(
-      {
-        ...notification,
-      },
-      undefined,
-      'notifications'
-    ) + 'RETURNING id, sender, receiver, message, sentAt, createdAt'
+  const query = pgp.helpers.insert(
+    {
+      ...notification,
+    },
+    undefined,
+    'notifications'
+  )
 
   return db
     .one(query)
