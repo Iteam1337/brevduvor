3f08ba9a15e7337506e2a5c8709134fc95ed9adb
faroutchris <georgiev.christo@gmail.com>
2019-11-25 17:06:00 +0100

    chore(api): debug ci

--- packages/api/lib/adapters/smsclient.ts
+++ packages/api/lib/adapters/smsclient.ts
@@ -1,6 +1,8 @@
 import twilio from 'twilio'
 import config from './../config'
 
+console.log('smsclient config -->', config)
+
 export const client = twilio(config.TWILIO.account, config.TWILIO.token)
 
 export const send = (message: string, receiver: string) => {
