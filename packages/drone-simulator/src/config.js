3b7df14becae0fd709379e71ee68e3a32965f70f
momentiris <momentiris@gmail.com>
2019-11-25 10:25:42 +0100

    fix: use correct docker-compose name in e2e.yml

--- packages/drone-simulator/src/config.js
+++ packages/drone-simulator/src/config.js
@@ -2,7 +2,7 @@ const config = require('@iteam/config')({
   file: `${__dirname}/../config.json`,
   defaults: {
     port: 3030,
-    osrm: process.env.OSRM_HOST || 'http://localhost:5000',
+    osrm: 'http://localhost:5000',
   },
 })
 
