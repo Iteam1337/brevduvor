1c08e5e02c897a6f2a22a4088a53effd5111794d
momentiris <momentiris@gmail.com>
2020-01-23 10:14:01 +0100

    chore: move apollo init from receiver to app

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -26,6 +26,21 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen'
 
+// import { useSubscription, gql } from '@apollo/client'
+
+// const HAS_STARTED_NOTIFICATION = gql`
+//   subscription hasStartedNotification {
+//     landingNotification {
+//       id
+//     }
+//   }
+// `
+
+// function App() {
+//   const { data, loading, error } = useSubscription(HAS_STARTED_NOTIFICATION)
+//   return <div className="App">Receiver</div>
+// }
+
 declare var global: { HermesInternal: null | {} }
 
 const App = () => {
--- packages/app/index.js
+++ packages/app/index.js
@@ -6,4 +6,44 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  getMainDefinition,
+  split,
+  HttpLink,
+} from '@apollo/client'
+
+import { WebSocketLink } from '@apollo/link-ws'
+
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_URI,
+})
+
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_GRAPHQL_WS_URI,
+  options: {
+    reconnect: true,
+  },
+})
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+})
+
 AppRegistry.registerComponent(appName, () => App)
