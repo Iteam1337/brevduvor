487837d0a664d2056fc341b47ee4456a3dae08bd
Simon Lundell <simon.lundell@iteam.se>
2020-02-05 11:16:41 +0100

    fix: remove comma

--- packages/drone/drone_api.py
+++ packages/drone/drone_api.py
@@ -1,6 +1,6 @@
 import airsim
 from flask import Flask, Response, request
-from flask_restful import Resource, Api,
+from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
 from airsimgeo import AirSimGeoClient
