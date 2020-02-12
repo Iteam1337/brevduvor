f054b7dcc152d4e547bf0d5c98dce9342c6953a9
izabellalarsson <izzwii.izabella@gmail.com>
2020-02-12 15:51:13 +0100

    style: 💄 picker style

--- packages/app/src/components/form/InputSelect.tsx
+++ packages/app/src/components/form/InputSelect.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
-import Heading from '~/components/typography/Heading'
+import styled from 'styled-components/native'
+import { View, StyleSheet } from 'react-native'
 import { Destination } from '~/__generated__/app'
 import RNPickerSelect from 'react-native-picker-select'
-
+import Label from '~/components/form/Label'
+import theme from '~/styles/theme'
 interface InputSelectProps {
   name?: string
+  label: string
   selectOptions?: Destination[]
   callback: (value: string) => void
   placeholder: string
 }
 
+const InputSelectWrapper = styled(View)`
+  width: 100%;
+  align-items: flex-start;
+`
+
 const InputSelect: React.FC<InputSelectProps> = ({
   selectOptions = [],
   callback,
-  name = '',
+  label,
   placeholder,
 }) => {
   const toSelectItem: (d: Destination) => { label: string; value: string } = ({
@@ -24,39 +32,34 @@ const InputSelect: React.FC<InputSelectProps> = ({
   })
 
   return (
-    <>
-      <Heading text={name} />
+    <InputSelectWrapper>
+      <Label value={label} />
       <RNPickerSelect
         placeholder={{ label: placeholder, value: null }}
         onValueChange={val => callback(val)}
         items={selectOptions?.map(toSelectItem)}
+        style={PickerStyle}
       />
-    </>
+    </InputSelectWrapper>
   )
 }
 
-// const IsnputSelect: React.FC<InputSelectProps> = ({
-//   select,
-//   callback,
-//   name = '',
-// }) => {
-//   return (
-//     <>
-//       <Heading text={name} />
-//       <Picker
-//         selectedValue={name}
-//         onValueChange={itemValue => {
-//           callback(itemValue)
-//         }}
-//       >
-//         <Picker.Item label="" value="" />
-//         {select &&
-//           select.map(destination => (
-//             <Picker.Item label={destination.alias} value={destination.alias} />
-//           ))}
-//       </Picker>
-//     </>
-//   )
-// }
+const PickerStyle = StyleSheet.create({
+  inputIOSContainer: {
+    width: 140,
+    height: 42,
+    borderWidth: 1,
+    borderColor: theme.colors.NGrey7,
+    fontFamily: theme.typography.paragraph,
+    fontSize: 16,
+    paddingLeft: 4,
+    marginBottom: theme.spacing.sm,
+    justifyContent: 'center',
+  },
+
+  inputIOS: {
+    fontSize: 16,
+  },
+})
 
 export default InputSelect
