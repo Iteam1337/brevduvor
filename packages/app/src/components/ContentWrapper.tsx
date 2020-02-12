739abf38dfb7e342add09a7194e706080263e02e
izabellalarsson <izzwii.izabella@gmail.com>
2020-02-12 15:49:40 +0100

    style: 💄 update styles and clean up

--- packages/app/src/components/ContentWrapper.tsx
+++ packages/app/src/components/ContentWrapper.tsx
@@ -2,23 +2,15 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 import styled from 'styled-components/native'
 
-const Wrapper = styled(SafeAreaView)<{
-  toLeft: ContentWrapperProps['toLeft']
-}>`
+const Wrapper = styled(SafeAreaView)`
   width: 100%;
-  align-items: ${({ toLeft }) => (toLeft ? 'flex-start' : 'center')};
+  align-items: center;
   justify-content: center;
   height: 90%;
 `
-interface ContentWrapperProps {
-  toLeft?: boolean
-}
 
-const ContentWrapper: React.FC<ContentWrapperProps> = ({
-  children,
-  toLeft,
-}) => {
-  return <Wrapper toLeft={toLeft}>{children}</Wrapper>
+const ContentWrapper: React.FC = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>
 }
 
 export default ContentWrapper
--- packages/app/src/views/Book.tsx
+++ packages/app/src/views/Book.tsx
@@ -9,12 +9,12 @@ import {
 import { INIT_DRONE } from '~/graphql/mutations'
 import { GET_ALL_DESTINATIONS } from '~/graphql/queries'
 import PrimaryButton from '~/components/Button'
-import Label from '~/components/form/Label'
 import ScrollableLayout from '~/components/ScrollableLayout'
 import InputSelect from '~/components/form/InputSelect'
 import ButtonWrapper from '~/components/ButtonWrapper'
 import ContentWrapper from '~/components/ContentWrapper'
 import Trip from '~/assets/Trip'
+import Heading from '~/components/typography/Heading'
 
 const backgroundImage = require('~/../assets/background-topo.png')
 
@@ -54,38 +54,38 @@ const Book: React.FC<BookProps> = ({ navigation }) => {
 
   return (
     <ScrollableLayout image={backgroundImage}>
-      <ContentWrapper toLeft={true}>
+      <ContentWrapper>
+        <Heading text="Boka transport" />
         <Trip />
-        <Label value="Från" />
         <InputSelect
+          label="Från"
           name={startValue}
           placeholder="Välj från"
           selectOptions={data?.allDestinations}
           callback={setStartValue}
         />
 
-        <Label value="Till" />
         <InputSelect
+          label="Till"
           name={stopValue}
           selectOptions={data?.allDestinations}
           callback={setStopValue}
           placeholder="Välj till"
         />
-
-        <ButtonWrapper>
-          <PrimaryButton
-            text="Avbryt"
-            callback={() => navigation.navigate('Home')}
-          />
-          <PrimaryButton
-            text="Nästa"
-            callback={() => {
-              initDrone()
-              navigation.navigate('BookingEta')
-            }}
-          />
-        </ButtonWrapper>
       </ContentWrapper>
+      <ButtonWrapper>
+        <PrimaryButton
+          text="Avbryt"
+          callback={() => navigation.navigate('Home')}
+        />
+        <PrimaryButton
+          text="Nästa"
+          callback={() => {
+            initDrone()
+            navigation.navigate('BookingEta')
+          }}
+        />
+      </ButtonWrapper>
     </ScrollableLayout>
   )
 }
