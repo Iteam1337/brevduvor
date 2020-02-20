9bda3b2243d1c811e27d343f67da18371295762e
Tjejjourenvast <49750140+Tjejjourenvast@users.noreply.github.com>
2020-02-20 14:47:59 +0100

    refactor: bookings start (#77)
    * refactor: 💡 rewrite booking start section logic a bit
    
    * refactor: 💡 use generic type for selectinput and rename file
    
    Co-authored-by: Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>

--- packages/app/src/components/form/Select.tsx
+++ packages/app/src/components/form/Select.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import styled from 'styled-components/native'
 import { View, StyleSheet } from 'react-native'
-import { Destination } from '~/__generated__/app'
+import { Destination, DestinationInput } from '~/__generated__/app'
 import RNPickerSelect from 'react-native-picker-select'
 import Label from '~/components/form/Label'
 import theme from '~/styles/theme'
-interface InputSelectProps {
+
+interface InputSelectProps<T = any> {
   name?: string
   label: string
   selectOptions?: Destination[]
-  callback: (value: string) => void
+  callback: (value: T) => void
   placeholder: string
 }
 
@@ -18,7 +19,7 @@ const InputSelectWrapper = styled(View)`
   align-items: flex-start;
 `
 
-const InputSelect: React.FC<InputSelectProps> = ({
+const Geo: React.FC<InputSelectProps<DestinationInput>> = ({
   selectOptions = [],
   callback,
   label,
@@ -31,13 +32,25 @@ const InputSelect: React.FC<InputSelectProps> = ({
     value: alias,
   })
 
+  const handleChange = (input: string) => {
+    const selectOption = selectOptions.find(({ alias }) => alias === input)
+    if (selectOption) {
+      const { alias, lat, lon } = selectOption // strip __typename prop
+
+      callback({ alias, lat, lon })
+    }
+  }
+
   return (
     <InputSelectWrapper>
       <Label value={label} />
       <RNPickerSelect
-        placeholder={{ label: placeholder, value: null }}
-        onValueChange={val => callback(val)}
-        items={selectOptions?.map(toSelectItem)}
+        placeholder={{
+          label: placeholder,
+          value: null,
+        }}
+        onValueChange={handleChange}
+        items={selectOptions.map(toSelectItem)}
         style={PickerStyle}
       />
     </InputSelectWrapper>
@@ -63,4 +76,4 @@ const PickerStyle = StyleSheet.create({
   },
 })
 
-export default InputSelect
+export default { Geo }
