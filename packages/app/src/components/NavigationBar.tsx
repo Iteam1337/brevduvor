bab6fbc99df2cff47d89dd85968f0db436bd4a48
izabellalarsson <izzwii.izabella@gmail.com>
2020-02-24 13:01:22 +0100

    refactor: 💡 highlight icons when active

--- packages/app/src/components/NavigationBar.tsx
+++ packages/app/src/components/NavigationBar.tsx
@@ -19,30 +19,35 @@ const BookingNavigation = () => {
     </HomeStack.Navigator>
   )
 }
+
 const NavigationBar = () => {
   return (
     <NavigationContainer>
-      <BottomMenu.Navigator tabBarOptions={{ showLabel: false }}>
+      <BottomMenu.Navigator
+        tabBarOptions={{ showLabel: false }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            let active = focused ? 1 : 0.4
+
+            if (route.name === 'Hem') return <HomeIcon active={active} />
+            if (route.name === 'Historik') {
+              return <HistoryIcon active={active} />
+            }
+            if (route.name === 'Notifikationer') {
+              return <NotificationIcon active={active} />
+            }
+            if (route.name === 'Inställningar')
+              return <SettingsIcon active={active} />
+          },
+        })}
+      >
+        <BottomMenu.Screen name="Hem" component={BookingNavigation} />
+        <BottomMenu.Screen name="Historik" component={BookingNavigation} />
         <BottomMenu.Screen
-          options={{ tabBarIcon: () => <HomeIcon /> }}
-          name="Hem"
-          component={BookingNavigation}
-        />
-        <BottomMenu.Screen
-          options={{ tabBarIcon: () => <HistoryIcon /> }}
-          name="Historik"
-          component={BookingNavigation}
-        />
-        <BottomMenu.Screen
-          options={{ tabBarIcon: () => <NotificationIcon /> }}
           name="Notifikationer"
           component={BookingNavigation}
         />
-        <BottomMenu.Screen
-          options={{ tabBarIcon: () => <SettingsIcon /> }}
-          name="Inställningar"
-          component={BookingNavigation}
-        />
+        <BottomMenu.Screen name="Inställningar" component={BookingNavigation} />
       </BottomMenu.Navigator>
     </NavigationContainer>
   )
