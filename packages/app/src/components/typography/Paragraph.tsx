8e028fd52e9c9befb2015d5ee393ad4f52ba3a10
izabellalarsson <izzwii.izabella@gmail.com>
2020-02-27 14:50:24 +0100

    refactor: 💡 clean up with extra inteface for specific styles

--- packages/app/src/components/typography/Paragraph.tsx
+++ packages/app/src/components/typography/Paragraph.tsx
@@ -2,33 +2,37 @@ import React from 'react'
 import styled from 'styled-components/native'
 import { Text } from 'react-native'
 
-interface ParagraphProps {
+interface ParagraphProps extends StyleProps {
   text: string
+  styles?: StyleProps
+}
+
+interface StyleProps {
   small?: boolean
   bold?: boolean
   toLeft?: boolean
+  toRight?: boolean
 }
 
-const Paragraph = styled(Text)<{
-  small: ParagraphProps['small']
-  toLeft: ParagraphProps['toLeft']
-  bold: ParagraphProps['bold']
-}>`
+const Paragraph = styled(Text)<StyleProps>`
   font-family: ${({ theme, bold }) =>
     bold ? theme.typography.bold : theme.typography.paragraph};
   font-size: ${({ theme, small }) =>
     small ? theme.typography.sizes.sm : theme.typography.sizes.md};
   color: ${({ theme }) => theme.colors.NGrey1};
   ${({ toLeft }) => toLeft && 'align-self: flex-start;'};
+  ${({ toRight }) =>
+    toRight && 'flex-grow: 1; text-align: right; margin-right: 10px;'}
 `
 
 const Component: React.FC<ParagraphProps> = ({
   text,
-  small = false,
-  bold = false,
-  toLeft = false,
+  small,
+  bold,
+  toLeft,
+  toRight,
 }) => (
-  <Paragraph bold={bold} toLeft={toLeft} small={small}>
+  <Paragraph bold={bold} toLeft={toLeft} toRight={toRight} small={small}>
     {text}
   </Paragraph>
 )
